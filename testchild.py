abb852e78aa9c4db82bede08f15a8987155dcadf
habeyo <59774460+habeyo@users.noreply.github.com>
2021-02-06 19:48:37 +0100

    Update testchild.py

--- testchild.py
+++ testchild.py
@@ -1,2 +1,4 @@
 # this is python test child to be tested and leter to be merged in to master
+# here i will test all the new staff and then i will merge this section to the master branch by a pull request.
+# a pull request means some one who is experienced will review what u have done and aprove and merge it to the master branch
 print('this is child_branch')
